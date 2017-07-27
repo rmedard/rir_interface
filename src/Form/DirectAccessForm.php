6b299c136ad8f7e5d43bd8c78d00b3c9534cd8e8
rmedard <00Derfine@5>
2017-07-27 23:40:02 +0200

    Kubuntu

--- src/Form/DirectAccessForm.php
+++ src/Form/DirectAccessForm.php
@@ -52,6 +52,19 @@ class DirectAccessForm extends FormBase {
   public function validateForm(array &$form, FormStateInterface $form_state) {
     if ($form_state->isValueEmpty('reference_number')){
       $form_state->setErrorByName('reference_number', t('Provide reference number'));
+    } else {
+      $reference = $form_state->get('reference_number');
+      $nodeQuery = \Drupal::entityQuery('node')
+        ->condition('type', 'advert')
+        ->condition('status', 1)
+        ->condition('field_advert_reference', $reference);
+      $node = $nodeQuery->execute();
+      if (isset($node)){
+        $url = Url::fromUri('internal:/advert/'.$reference);
+        $form_state->setRedirectUrl($url);
+      } else {
+        drupal_set_message($this->t("Sorry, no advert found"), 'error');
+      }
     }
   }
 
@@ -63,20 +76,21 @@ class DirectAccessForm extends FormBase {
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The current state of the form.
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $reference = $form_state->get('reference_number');
-    var_dump("dore" .$reference);
-    die();
-    $nodeQuery = \Drupal::entityQuery('node')
-      ->condition('type', 'advert')
-      ->condition('status', 1)
-      ->condition('field_advert_reference', $reference);
-    $node = $nodeQuery->execute();
-    if (isset($node)){
-      $url = Url::fromUri('internal:/advert/QL7XT9TBN');
-      $form_state->setRedirectUrl($url);
-    } else {
-      drupal_set_message($this->t("Sorry, no advert found"), 'error');
-    }
-  }
+//  public function submitForm(array &$form, FormStateInterface $form_state) {
+//    $element = $form['reference_number'];
+//    $reference = $form_state->get('reference_number');
+//    var_dump("dore" .$reference);
+//    die();
+//    $nodeQuery = \Drupal::entityQuery('node')
+//      ->condition('type', 'advert')
+//      ->condition('status', 1)
+//      ->condition('field_advert_reference', $reference);
+//    $node = $nodeQuery->execute();
+//    if (isset($node)){
+//      $url = Url::fromUri('internal:/advert/QL7XT9TBN');
+//      $form_state->setRedirectUrl($url);
+//    } else {
+//      drupal_set_message($this->t("Sorry, no advert found"), 'error');
+//    }
+//  }
 }
