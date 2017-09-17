b134a7f3ba40b40af3bbe4917c020d10aa1cc920
rmedard <00Derfine@5>
2017-09-17 17:12:36 +0200

    Kubuntu

--- src/Plugin/Block/RiRAdvertDetails.php
+++ src/Plugin/Block/RiRAdvertDetails.php
@@ -69,9 +69,9 @@ class RiRAdvertDetails extends BlockBase {
                 'widget' => array( 'name' => 'oxygen', 'css' => drupal_get_path('module', 'fivestar') . '/widgets/oxygen/oxygen.css' )
               );
 
-              $fivestar_values = fivestar_get_votes('node', $advertiser->id());
-              //$render_form = drupal_get_form('fivestar_custom_widget', $fivestar_values, $settings);
-              $ratings = \Drupal::formBuilder()->getForm('Drupal\fivestar\Plugin\Field\FieldWidget', $fivestar_values, $settings);
+//              $fivestar_values = fivestar_get_votes('node', $advertiser->id());
+//              //$render_form = drupal_get_form('fivestar_custom_widget', $fivestar_values, $settings);
+//              $ratings = \Drupal::formBuilder()->getForm('Drupal\fivestar\Plugin\Field\FieldWidget', $fivestar_values, $settings);
             }
         }
 
