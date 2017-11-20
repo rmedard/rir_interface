eddb48b120f14214a4bdaf236b2ca3d3c09de981
rmedard <00Derfine@5>
2017-11-20 23:07:13 +0100

    Win10

---
+++ src/Plugin/Block/HiRLikeButtonsBlock.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medar
+ * Date: 20/11/2017
+ * Time: 23:03
+ */
+
+namespace Drupal\rir_interface\Plugin\Block;
+
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Class HiRLikeButtonsBlock
+ * @package Drupal\rir_interface\Plugin\Block
+ * @Block(
+ *   id = "hir_like_buttons_block",
+ *   admin_label = @Translation("HiR Like Buttons Block"),
+ *   category = @Translation("Custom RIR Blocks")
+ * )
+ */
+class HiRLikeButtonsBlock extends BlockBase
+{
+
+    /**
+     * Builds and returns the renderable array for this block plugin.
+     *
+     * If a block should not be rendered because it has no content, then this
+     * method must also ensure to return no content: it must then only return an
+     * empty array, or an empty array with #cache set (with cacheability metadata
+     * indicating the circumstances for it being empty).
+     *
+     * @return array
+     *   A renderable array representing the content of the block.
+     *
+     * @see \Drupal\block\BlockViewBuilder
+     */
+    public function build()
+    {
+        return[
+            '#theme' => 'hir_like_buttons',
+        ];
+    }
+}
