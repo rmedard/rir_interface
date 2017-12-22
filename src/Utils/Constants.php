9329e67ca658de2b0b5f1c8dc2e77466ba00b5c6
rmedard <00Derfine@5>
2017-12-22 01:44:25 +0100

    Win10

---
+++ src/Utils/Constants.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: medar
+ * Date: 22/12/2017
+ * Time: 01:39
+ */
+
+namespace Drupal\rir_interface\Utils;
+
+
+interface Constants
+{
+    const CURRENCY_USD_RWF_RATE = 'latest_usd_rwf_rate';
+}
