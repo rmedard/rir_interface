23431037ecb17df381a28f63f5e3b21affd77c09
Medard Rebero <medard.rebero@gmail.com>
2020-06-28 21:56:26 +0200

    W10

---
+++ src/Plugin/Block/GooglePlacesApiAttributionBlock.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Drupal\rir_interface\Plugin\Block;
+
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Fax' block.
+ *
+ * @Block(
+ *   id = "geolocation_google_places_api_attribution_block",
+ *   admin_label = @Translation("Geolocation - Google Places API Attribution block"),
+ * )
+ */
+class GooglePlacesApiAttributionBlock extends BlockBase
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function build()
+    {
+        return ['#markup' => '<span id="geolocation-google-places-api-attribution"></span>'];
+    }
+}
