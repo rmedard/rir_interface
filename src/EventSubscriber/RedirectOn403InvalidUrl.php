56fccb1499cde26fc1ed631351cf9764eb630148
Medard Rebero <medard.rebero@gmail.com>
2021-02-16 18:00:11 +0100

    Refactor

---
+++ src/EventSubscriber/RedirectOn403InvalidUrl.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\rir_interface\EventSubscriber;
+
+
+use Drupal;
+use Drupal\Core\EventSubscriber\HttpExceptionSubscriberBase;
+use Drupal\Core\Routing\RequestHelper;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class RedirectOn403InvalidUrl extends HttpExceptionSubscriberBase
+{
+
+    protected function getHandledFormats(): array
+    {
+        return [];
+    }
+
+    public function on403(GetResponseForExceptionEvent $event) {
+        if (!RequestHelper::isCleanUrl($event->getRequest())) {
+            Drupal::logger('rir_interface')->warning('Invalid Url with /index.php detected.');
+            $cleanRequestUri = $this->cleanPath($event->getRequest()->getRequestUri());
+            $response = new TrustedRedirectResponse($cleanRequestUri, 302);
+            $response->headers->set('X-Drupal-Route-Normalizer', 1);
+            $event->setResponse($response);
+        }
+    }
+
+    private function cleanPath($pathStr): string {
+        if (substr($pathStr, 0, 10) === '/index.php') {
+            return $this->cleanPath(substr_replace($pathStr, '', 0, 10));
+        }
+        return $pathStr;
+    }
+}
