cf58dc4996a2ed0c2f0a83bafba0c245cb4b6214
Medard Rebero <medard.rebero@gmail.com>
2021-03-14 15:51:53 +0100

    refactor

--- src/Utils/Constants.php
+++ src/Utils/Constants.php
@@ -13,7 +13,9 @@ interface Constants
 {
     const USD_RWF_EXCHANGE_RATE = 'latest_usd_rwf_rate';
     const LATEST_DAY_EXCHANGE_RATE = 'latest_day_exchange_rate';
+    const ADVERT_CREATED = 'advert_insert_alert';
     const ADVERT_VALIDATED = 'advert_first_published';
     const ADVERT_VALIDATED_NOTIFY_PR = 'advert_validated_notify_pr';
     const PROPOSED_ADVERTS_TO_PR = 'proposed_adverts_to_pr';
+    const CAMPAIGN_ALERT_EMAIL = 'send_campaign_email';
 }
