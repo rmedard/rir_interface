6d1817dc61d634380020154eb1836a470a468002
Rebero <rmedard5@gmail.com>
2022-04-06 12:20:27 +0200

    price input formatter

--- js/custom_interface.js
+++ js/custom_interface.js
@@ -3,51 +3,29 @@
     attach: function (context, settings) {
 
       const priceField = $(context).find('input#edit-field-advert-price-0-value');
-      if (priceField && priceField.length) {
-        priceField.attr('type', 'text');
-        priceField.number(true, 0);
-        $(context).find('form#node-advert-edit-form').submit(function() {
-          const value  = priceField.val();
-          priceField.attr('type', 'number');
-          priceField.val(value);
-        });
-        $(context).find('form#node-advert-form').submit(function() {
-          const value  = priceField.val();
-          priceField.attr('type', 'number');
-          priceField.val(value);
-        });
-      }
+      numbifyField(priceField);
 
       const startingValueField = $(context).find('input#edit-field-advert-bid-starting-value-0-value');
-      if (startingValueField && startingValueField.length) {
-        startingValueField.attr('type', 'text');
-        startingValueField.number(true, 0);
-        $(context).find('form#node-advert-edit-form').submit(function() {
-          const value  = startingValueField.val();
-          startingValueField.attr('type', 'number');
-          startingValueField.val(value);
-        });
-        $(context).find('form#node-advert-form').submit(function() {
-          const value  = startingValueField.val();
-          startingValueField.attr('type', 'number');
-          startingValueField.val(value);
-        });
-      }
+      numbifyField(startingValueField);
 
       const securityAmountField = $(context).find('input#edit-field-advert-bid-security-amount-0-value');
-      if (securityAmountField && securityAmountField.length) {
-        securityAmountField.attr('type', 'text');
-        securityAmountField.number(true, 0);
-        $(context).find('form#node-advert-edit-form').submit(function() {
-          const value  = securityAmountField.val();
-          securityAmountField.attr('type', 'number');
-          securityAmountField.val(value);
-        });
-        $(context).find('form#node-advert-form').submit(function() {
-          const value  = securityAmountField.val();
-          securityAmountField.attr('type', 'number');
-          securityAmountField.val(value);
-        });
+      numbifyField(securityAmountField);
+
+      function numbifyField(field) {
+        if (field && field.length) {
+          field.attr('type', 'text');
+          field.number(true, 0);
+          $(context).find('form#node-advert-edit-form').submit(function () {
+            const value = field.val();
+            field.attr('type', 'number');
+            field.val(value);
+          });
+          $(context).find('form#node-advert-form').submit(function () {
+            const value = field.val();
+            field.attr('type', 'number');
+            field.val(value);
+          });
+        }
       }
     }
   }
