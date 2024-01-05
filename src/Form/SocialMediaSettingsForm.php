a0510953f553e0dfe80283f659f06edec6600a50
medard <medard.rebero@gmail.com>
2024-01-05 15:36:04 +0100

    Add whatsapp share button

--- src/Form/SocialMediaSettingsForm.php
+++ src/Form/SocialMediaSettingsForm.php
@@ -26,37 +26,43 @@ class SocialMediaSettingsForm extends ConfigFormBase
     {
         $config = $this->config(static::SETTINGS);
         $form['linkedin_page'] = [
-            '#type' => 'textfield',
+            '#type' => 'url',
             '#title' => $this->t('Linkedin page'),
             '#default_value' => $config->get('linkedin_page'),
             '#description' => $this->t('The full URL to the target linkedin page')
         ];
         $form['facebook_page'] = [
-            '#type' => 'textfield',
+            '#type' => 'url',
             '#title' => $this->t('Facebook page'),
             '#default_value' => $config->get('facebook_page'),
             '#description' => $this->t('The full URL to the target facebook page')
         ];
         $form['twitter_page'] = [
-            '#type' => 'textfield',
+            '#type' => 'url',
             '#title' => $this->t('Twitter page'),
             '#default_value' => $config->get('twitter_page'),
             '#description' => $this->t('The full URL to the target twitter page')
         ];
         $form['instagram_page'] = [
-            '#type' => 'textfield',
+            '#type' => 'url',
             '#title' => $this->t('Instagram page'),
             '#default_value' => $config->get('instagram_page'),
             '#description' => $this->t('The full URL to the target instagram page')
         ];
         $form['youtube_page'] = [
-            '#type' => 'textfield',
+            '#type' => 'url',
             '#title' => $this->t('Youtube page'),
             '#default_value' => $config->get('youtube_page'),
             '#description' => $this->t('The full URL to the target youtube page')
         ];
+        $form['whatsapp_call_link'] = [
+          '#type' => 'url',
+          '#title' => $this->t('Whatsapp call link'),
+          '#default_value' => $config->get('whatsapp_call_link'),
+          '#description' => $this->t('The full link for whatsapp call')
+        ];
         $form['email_banner_image'] = [
-          '#type' => 'textfield',
+          '#type' => 'url',
           '#title' => $this->t('Email banner image'),
           '#default_value' => $config->get('email_banner_image'),
           '#description' => $this->t('The full URL to the email banner image')
@@ -64,55 +70,14 @@ class SocialMediaSettingsForm extends ConfigFormBase
         return parent::buildForm($form, $form_state);
     }
 
-    public function validateForm(array &$form, FormStateInterface $form_state)
-    {
-        if (!$form_state->isValueEmpty('linkedin_page')) {
-            $isValid = filter_var($form_state->getValue('linkedin_page'), FILTER_VALIDATE_URL);
-            if ($isValid === false) {
-                $form_state->setErrorByName('linkedin_page', t('This must be a valid URL starting with http/https'));
-            }
-        }
-        if (!$form_state->isValueEmpty('facebook_page')) {
-            $isValid = filter_var($form_state->getValue('facebook_page'), FILTER_VALIDATE_URL);
-            if ($isValid === false) {
-                $form_state->setErrorByName('facebook_page', t('This must be a valid URL starting with http/https'));
-            }
-        }
-        if (!$form_state->isValueEmpty('twitter_page')) {
-            $isValid = filter_var($form_state->getValue('twitter_page'), FILTER_VALIDATE_URL);
-            if ($isValid === false) {
-                $form_state->setErrorByName('twitter_page', t('This must be a valid URL starting with http/https'));
-            }
-        }
-        if (!$form_state->isValueEmpty('instagram_page')) {
-            $isValid = filter_var($form_state->getValue('instagram_page'), FILTER_VALIDATE_URL);
-            if ($isValid === false) {
-                $form_state->setErrorByName('instagram_page', t('This must be a valid URL starting with http/https'));
-            }
-        }
-        if (!$form_state->isValueEmpty('youtube_page')) {
-            $isValid = filter_var($form_state->getValue('youtube_page'), FILTER_VALIDATE_URL);
-            if ($isValid === false) {
-                $form_state->setErrorByName('youtube_page', t('This must be a valid URL starting with http/https'));
-            }
-        }
-        if (!$form_state->isValueEmpty('email_banner_image')) {
-          $isValid = filter_var($form_state->getValue('email_banner_image'), FILTER_VALIDATE_URL);
-          if ($isValid === false) {
-            $form_state->setErrorByName('email_banner_image', t('This must be a valid URL starting with http/https'));
-          }
-        }
-        parent::validateForm($form, $form_state);
-    }
-
-    public function submitForm(array &$form, FormStateInterface $form_state)
-    {
+    public function submitForm(array &$form, FormStateInterface $form_state): void {
         $this->configFactory->getEditable(static::SETTINGS)
             ->set('linkedin_page', $form_state->getValue('linkedin_page'))
             ->set('facebook_page', $form_state->getValue('facebook_page'))
             ->set('twitter_page', $form_state->getValue('twitter_page'))
             ->set('instagram_page', $form_state->getValue('instagram_page'))
             ->set('youtube_page', $form_state->getValue('youtube_page'))
+            ->set('whatsapp_call_link', $form_state->getValue('whatsapp_call_link'))
             ->set('email_banner_image', $form_state->getValue('email_banner_image'))
             ->save();
         parent::submitForm($form, $form_state);
